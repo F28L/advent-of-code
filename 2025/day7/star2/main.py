cd1f772b95c59b2edfee4e3515f61fa0847fecda
Sandeep Ramesh <cfcsandeep@gmail.com>
2025-12-07 15:48:01 -0500

    day 7 star 2

--- 2025/day7/star2/main.py
+++ 2025/day7/star2/main.py
@@ -1,58 +1,35 @@
 import sys
 
-
 def run():
-    print('Reading input...')
-    txt = sys.stdin.read().strip()
-    ranges, ids = txt.split('\n\n')
-    print('Finished reading input...')
-    print('Defining fresh id ranges...')
-    fresh_ids = []
-    for r in ranges.strip().split('\n'):
-        print('Processing range:', r)
-        left, right = r.split('-')
-        i = 0
-        while i < len(fresh_ids):
-            id_range = fresh_ids[i]
-            print('Comparing to range:', id_range)
-            if not (int(right) < id_range[0] or int(left) > id_range[1]):
-                # Overlap so merge range
-                print('Overlaps with existing range, merging.')
-                left = str(min(int(left), id_range[0]))
-                right = str(max(int(right), id_range[1]))
-                fresh_ids[i] = (int(left), int(right))
-                print('New merged range:', fresh_ids[i])
-                break
-            i += 1
-        # if we couldnt merge it in, add it to its own range
-        if i == len(fresh_ids):
-            print('No overlap found, adding new range.')
-            fresh_ids.append((int(left), int(right)))
-        print('Now have ranges:', fresh_ids)
-        print('Checking for overlapping ranges...')
-        j = 0
-        while j < len(fresh_ids) - 1:
-            k = j + 1
-            while k < len(fresh_ids):
-                range_j = fresh_ids[j]
-                range_k = fresh_ids[k]
-                if not (range_j[1] < range_k[0] or range_j[0] > range_k[1]):
-                    # overlap found so merge
-                    new_left = min(range_j[0], range_k[0])
-                    new_right = max(range_j[1], range_k[1])
-                    fresh_ids[j] = (new_left, new_right)
-                    del fresh_ids[k]
-                else:
-                    k += 1
-            j += 1
-    print('Finished defining unique fresh ids.')
+    board = sys.stdin.read().strip().split("\n")
+    max_rows = len(board)
+    max_cols = len(board[0])
 
-    total_fresh_ids = 0
-    for r in fresh_ids:
-        total_fresh_ids += r[1] - r[0] + 1
+    paths = [[0]*max_cols for _ in range(max_rows)]
 
-    return total_fresh_ids
+    for c in range(max_cols):
+        if board[0][c] == "S":
+            paths[0][c] = 1
+            break
 
+    for r in range(max_rows-1):
+        for c in range(max_cols):
+            count = paths[r][c]
+            if count == 0:
+                continue
+
+            below = board[r+1][c]
+
+            if below == "^":
+                if c-1 >= 0:
+                    paths[r+1][c-1] += count
+                if c+1 < max_cols:
+                    paths[r+1][c+1] += count
+            else:
+                paths[r+1][c] += count
+
+    return paths
 
 if __name__ == "__main__":
-    print(run())
+    p = run()
+    print(sum(p[-1]))
